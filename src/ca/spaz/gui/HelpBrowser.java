fe4c931167b2823c34b01e6cbf6f6d58745ba107
Artichikin <Artichikin@6e31c603-d216-0410-955d-b262f994ffaf>
2007-02-07 22:01:20 +0000

    help with xml contents (not finished)
    git-svn-id: http://svn.code.sf.net/p/cronometer/code/trunk/CRONOMETER@122 6e31c603-d216-0410-955d-b262f994ffaf

--- src/ca/spaz/gui/HelpBrowser.java
+++ src/ca/spaz/gui/HelpBrowser.java
@@ -2,20 +2,29 @@ package ca.spaz.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.io.File;
+import java.io.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class HelpBrowser extends JFrame {
 
    private WebViewer viewer;
    private File base;
-  
+   private JTree contents;
+   private JSplitPane splitPane;
+   private JScrollPane contentsScrollPane;
+   private Page contentsModel;
+   
    public HelpBrowser(String title, File base) {
       this.setTitle(title);
       this.base = base;
       setLayout(new BorderLayout());      
-      add(getViewer(), BorderLayout.CENTER); 
+      add(getSplitPane(), BorderLayout.CENTER); 
       setPage("index.html");
       pack();
       setVisible(true);
@@ -28,8 +37,102 @@ public class HelpBrowser extends JFrame {
    public WebViewer getViewer() {
       if (viewer == null) {
          viewer = new WebViewer();
-         viewer.setPreferredSize(new Dimension(600,400));
+         viewer.setPreferredSize(new Dimension(600,500));
       }
       return viewer;
    }
+   
+   
+   private JSplitPane getSplitPane() {
+      if (splitPane == null) {
+         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, getContentsPanel(), getViewer());
+      }
+      return splitPane;
+   }
+   
+
+   private JScrollPane getContentsPanel() {
+      if (contentsScrollPane == null) {
+         contentsScrollPane = new JScrollPane(getContents());
+         contentsScrollPane.setPreferredSize(new Dimension(200,500));
+
+      }
+      return contentsScrollPane;
+   }
+
+   private JTree getContents() {
+      if (contents == null) {
+         contents = new JTree(getHelpContentsModel());
+         contents.setBorder(BorderFactory.createEmptyBorder(6,6,6,6));
+      }
+      return contents;
+   }
+   
+   private Page getHelpContentsModel() {
+      if (contentsModel == null) {
+         try {
+            FileInputStream in = new FileInputStream(new File(base, "help.xml"));
+            contentsModel = loadContents(in);
+            in.close();
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+      }
+      return contentsModel;
+   }
+
+   private Page loadContents(InputStream in) throws ParserConfigurationException, SAXException, IOException {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      dbf.setNamespaceAware(true);
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      Document d = db.parse(in);
+      Element e = d.getDocumentElement();
+      return new Page(e);
+  }
+   
+   
+   protected class Page extends DefaultMutableTreeNode {
+      private String title;
+      private String url;
+   
+      public Page(Element e) {
+         super();
+         load(e);
+      }   
+      public Page(String title, String url) {
+         super();
+         this.title = title;
+         this.url = url;
+      }
+      public String getTitle() {
+         return title;
+      }
+      public void setTitle(String title) {
+         this.title = title;
+      }
+      public String getUrl() {
+         return url;
+      }
+      public void setUrl(String url) {
+         this.url = url;
+      }     
+      public String toString() {
+         return title;
+      }
+      public void load(Element e){
+         title = e.getAttribute("title");
+         url = e.getAttribute("url");
+         NodeList nl = e.getChildNodes();
+         for (int i=0; i<nl.getLength(); i++) {
+            if (nl.item(i).getNodeType()== Node.ELEMENT_NODE) {
+               if (((Element)nl.item(i)).getTagName().equals("page")) {
+                  add(new Page((Element)nl.item(i)));
+               }
+            }
+         }
+      }
+   }
+
+   
+   
 }
