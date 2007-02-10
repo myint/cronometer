66db8afcbed996b79092146c815c832b0bc3a7a3
Artichikin <Artichikin@6e31c603-d216-0410-955d-b262f994ffaf>
2007-02-10 17:57:46 +0000

    new help
    git-svn-id: http://svn.code.sf.net/p/cronometer/code/trunk/CRONOMETER@127 6e31c603-d216-0410-955d-b262f994ffaf

--- src/ca/spaz/gui/HelpBrowser.java
+++ src/ca/spaz/gui/HelpBrowser.java
@@ -1,11 +1,15 @@
 package ca.spaz.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.*;
 import java.io.*;
 
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.*;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLFrameHyperlinkEvent;
+import javax.swing.tree.*;
 import javax.xml.parsers.*;
 
 import org.w3c.dom.*;
@@ -13,49 +17,83 @@ import org.xml.sax.SAXException;
 
 public class HelpBrowser extends JFrame {
 
-   private WebViewer viewer;
    private File base;
+   private String baseTitle;
+   private JEditorPane htmlPane;
    private JTree contents;
    private JSplitPane splitPane;
    private JScrollPane contentsScrollPane;
    private Page contentsModel;
    
    public HelpBrowser(String title, File base) {
-      this.setTitle(title);
+      this.baseTitle = title;
+      this.setTitle(baseTitle);
       this.base = base;
-      setLayout(new BorderLayout());      
-      add(getSplitPane(), BorderLayout.CENTER); 
+      setLayout(new BorderLayout());
+      getContentPane().add(getSplitPane(), BorderLayout.CENTER);
       setPage("index.html");
       pack();
-      setVisible(true);
+   }   
+
+   /**
+    * Show window normally
+    */
+   public void showWindow() {
+      setVisible(true); 
+      toFront();
    }
    
-   public void setPage(String name) {
-      getViewer().setHTML(new File(base, name));
+   private JScrollPane makeHTMLScrollPane() {     
+      JScrollPane jsp = new JScrollPane(getViewer());
+      jsp.setBorder(new BevelBorder(BevelBorder.LOWERED)); 
+      jsp.setPreferredSize(new Dimension(600,500));
+      return jsp;
    }
    
-   public WebViewer getViewer() {
-      if (viewer == null) {
-         viewer = new WebViewer();
-         viewer.setPreferredSize(new Dimension(600,500));
+   public JEditorPane getViewer() {
+      if (htmlPane == null) {
+         htmlPane = new JEditorPane();
+         htmlPane.setPreferredSize(new Dimension(600,500));
+         htmlPane.setContentType("text/html");
+         htmlPane.setEditable(false);
+         htmlPane.setBorder(new EmptyBorder(8, 8, 8, 8));
+         htmlPane.addHyperlinkListener(new HyperlinkListener() {
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+               if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                  if (e instanceof HTMLFrameHyperlinkEvent) {
+                     HTMLFrameHyperlinkEvent evt = (HTMLFrameHyperlinkEvent) e;
+                     HTMLDocument doc = (HTMLDocument) htmlPane.getDocument();
+                     doc.processHTMLFrameHyperlinkEvent(evt);
+                  } else {
+                     try {
+                        if (e.getURL().getProtocol().equals("file")) {
+                           htmlPane.setPage(e.getURL());
+                        } else {
+                           Desktop.getDesktop().browse(e.getURL().toURI());
+                        }
+                     } catch (Exception ex) {
+                        ex.printStackTrace();
+                     }
+                  }
+               }
+            }
+         });
       }
-      return viewer;
+      return htmlPane;
    }
-   
-   
+      
    private JSplitPane getSplitPane() {
       if (splitPane == null) {
-         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, getContentsPanel(), getViewer());
+         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, getContentsPanel(), makeHTMLScrollPane());
+         splitPane.setBorder(BorderFactory.createEmptyBorder(6,6,6,6));             
       }
       return splitPane;
-   }
-   
+   }   
 
    private JScrollPane getContentsPanel() {
       if (contentsScrollPane == null) {
          contentsScrollPane = new JScrollPane(getContents());
          contentsScrollPane.setPreferredSize(new Dimension(200,500));
-
       }
       return contentsScrollPane;
    }
@@ -63,11 +101,45 @@ public class HelpBrowser extends JFrame {
    private JTree getContents() {
       if (contents == null) {
          contents = new JTree(getHelpContentsModel());
-         contents.setBorder(BorderFactory.createEmptyBorder(6,6,6,6));
+         contents.setBorder(BorderFactory.createEmptyBorder(6,6,6,6));         
+         contents.getSelectionModel().setSelectionMode( TreeSelectionModel.SINGLE_TREE_SELECTION);
+         contents.expandPath(new TreePath(getHelpContentsModel()));
+         int row = 0;
+         while (row < contents.getRowCount()) {
+            contents.expandRow(row++);
+         }
+         contents.addTreeSelectionListener(new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent e) {
+               setPage((Page)e.getPath().getLastPathComponent()); 
+            }
+         });          
       }
       return contents;
    }
    
+
+   public void setPage(String name) {
+      File f = new File(base, name);
+      if (f.exists()) {
+         try {
+            getViewer().setContentType("text/html");
+            getViewer().setPage(f.toURI().toURL());
+         } catch (IOException e) { 
+            e.printStackTrace();
+         }
+      } else {
+         getViewer().setContentType("text/html");   
+         getViewer().setText("<html><h3 align=\"center\">File Not Found: "+name+"</h3></html>");
+      }
+   }
+   
+   protected void setPage(Page page) {
+      if (page != null) {
+         setTitle(baseTitle + " - " + page.getTitle() + " - " + page.getUrl());
+         setPage(page.getUrl());      
+      }
+   }
+
    private Page getHelpContentsModel() {
       if (contentsModel == null) {
          try {
@@ -90,8 +162,40 @@ public class HelpBrowser extends JFrame {
       return new Page(e);
   }
    
+
+   public void showPage(String url) {
+      Page page = findPage(url, getHelpContentsModel());
+      if (page != null) {
+         selectPageInTree(page);         
+      }
+   }
    
-   protected class Page extends DefaultMutableTreeNode {
+   private Page findPage(String url) {
+      return findPage(url, getHelpContentsModel());
+   }
+   
+   private Page findPage(String url, Page parent) {
+      if (parent.getUrl().equals(url)) {
+         return parent;
+      }    
+      for (int i=0; i<parent.getChildCount(); i++) {
+         Page child = findPage(url, (Page)parent.getChildAt(i));
+         if (child != null) {
+            return child;
+         }         
+      }
+      return null;
+   }
+   
+
+   private void selectPageInTree(Page page) {
+      TreePath path = new TreePath(page.getPath());
+      getContents().expandPath(path);
+      getContents().setSelectionPath(path);
+      getContents().scrollPathToVisible(path); 
+   }
+    
+   private class Page extends DefaultMutableTreeNode {
       private String title;
       private String url;
    
