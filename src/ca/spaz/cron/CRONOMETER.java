bec5db1b7e32f50082ec7483a37f9aed25c5d037
Steven Myint <git@stevenmyint.com>
2014-05-11 17:42:22 -0700

    Remove update check

--- src/ca/spaz/cron/CRONOMETER.java
+++ src/ca/spaz/cron/CRONOMETER.java
@@ -458,16 +458,7 @@ public class CRONOMETER extends JFrame implements TaskListener, MRJQuitHandler, 
       public void run() {
          ToolBox.sleep(1000);
          Datasources.initialize(this);
-         if (UserManager.getUserManager().getCheckForUpdates()) {
-            Thread t = new Thread( new Runnable() {
-               public void run() {
-                  CRONOMETER.getInstance().doCheckForUpdates();
-               }
-            });
-            t.start();
-         }
       }
-
 
       public void progressStart() {
          prog = 0;
@@ -479,37 +470,6 @@ public class CRONOMETER extends JFrame implements TaskListener, MRJQuitHandler, 
 
       public void progress(int percent) {
          prog = percent;
-      }
-   }
-
-   public void doCheckForUpdates() {
-      try {
-         URL url = new URL("http://spaz.ca/cronometer/updates.xml");
-         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-         DocumentBuilder db = dbf.newDocumentBuilder();
-         Document d = db.parse(url.openStream());
-         Element e = d.getDocumentElement();
-         int b = XMLNode.getInt(e, "build");
-         if (b > BUILD) {
-            final String www = e.getAttribute("url");
-            SwingUtilities.invokeLater(new Runnable() {
-               public void run() {
-            	   int result = JOptionPane.showOptionDialog(CRONOMETER.getInstance(),
-            			   "A new version of " + TITLE + " is available!",
-                           "New Version",
-                           JOptionPane.OK_CANCEL_OPTION,
-                           JOptionPane.INFORMATION_MESSAGE,
-                           null,
-                           null,
-                           null);
-                  if (result == JOptionPane.OK_OPTION  && www != null) {
-                     ToolBox.launchURL(CRONOMETER.mainFrame, www);
-                  }
-               }
-            });
-         }
-      } catch (Exception ex) {
-         Logger.error(ex);
       }
    }
 
