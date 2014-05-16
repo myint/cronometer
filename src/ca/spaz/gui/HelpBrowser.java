f7973eb6bff9b1768cf4e0cd64ec094cf774ff4d
Steven Myint <git@stevenmyint.com>
2014-05-16 12:31:23 -0700

    Close on escape

--- src/ca/spaz/gui/HelpBrowser.java
+++ src/ca/spaz/gui/HelpBrowser.java
@@ -2,6 +2,9 @@ package ca.spaz.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -46,6 +49,16 @@ public class HelpBrowser extends JFrame {
         this.getContentPane().add(getSplitPane(), BorderLayout.CENTER);
         this.setPage("index.html");
         this.pack();
+
+        // Add escape listener to dismiss window.
+        getRootPane().registerKeyboardAction(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    setVisible(false);
+                }
+            },
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+            JComponent.WHEN_IN_FOCUSED_WINDOW);
     }
 
     /**
